c74577c7f8fb2d439c600d87d67733588c2f2c8f
user.name <atharmail12@gmail.com>
2022-03-12 12:26:13 +0530

    added page management and added title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Navs from "./Components/Navs";
+
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/starred" element={<Starred />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/starred" element={<Starred />} />
+    </Routes>
   );
 }
 
