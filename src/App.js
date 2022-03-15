3a16b58496b16ee5f1fb07ec01264e1d98e93749
user.name <atharmail12@gmail.com>
2022-03-15 18:17:12 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
+import Show from "./pages/Show";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/starred" element={<Starred />} />
+      <Route path="/show/:id" element={<Show />} />
     </Routes>
   );
 }
